01bfe8b6a12cc59806fc0f62b331f55d6eae2f26
Maxwel Olande <maxolande23@gmail.com>
2023-12-08 11:15:57 +0300

    Added the Dockerfile to the project

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+!@^(8(-le&obc(5@py1tz6z)aurusi*#m6mw0(ehkn$1_)33&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
